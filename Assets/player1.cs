cad895f8ce691d445a6094b9fb5a6530096352d6
Hayati Kaya <hayati.kaya@campus.netkent.edu.tr>
2021-01-23 16:40:15 +0300

    Proje dosyası ekle.

---
+++ Assets/player1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float hiz;
+
+    
+    
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0f, hiz * Time.deltaTime, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0f, -hiz * Time.deltaTime, 0f);
+        }
+    }
+}
---
+++ Assets/tophareketleri.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tophareketleri : MonoBehaviour
+{
+    public Rigidbody2D top;
+    public float xHizi, yHizi;
+
+
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D temas)
+    {
+        if (temas.gameObject.tag == "Player1")
+        {
+            top.velocity = new Vector2(-xHizi, Random.Range(-3f,3f));
+
+    }
+    else if (temas.gameObject.tag == "Player2")
+        {
+            top.velocity = new Vector2(xHizi, Random.Range(-3f, 3f));
+        }
+
+        if(temas.gameObject.tag == "UstDuvar") {
+            top.velocity = new Vector2(top.velocity.x, -yHizi); 
+        } else if (temas.gameObject.tag == "AltDuvar")
+        {
+            top.velocity = new Vector2(top.velocity.x, yHizi);
+        }
+    }
+}
+
